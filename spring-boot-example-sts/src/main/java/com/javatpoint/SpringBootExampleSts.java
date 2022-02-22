2ce6a4f1f691f1c2626ac85b226cd503be161f44
Kavyac34 <kavyachaluvadi34@gmail.com>
2022-02-22 19:05:19 +0530

    some changes made

--- spring-boot-example-sts/src/main/java/com/javatpoint/SpringBootExampleSts.java
+++ spring-boot-example-sts/src/main/java/com/javatpoint/SpringBootExampleSts.java
@@ -1,7 +1,7 @@
 package com.javatpoint;
 import org.springframework.boot.SpringApplication;  
 import org.springframework.boot.autoconfigure.SpringBootApplication; 
-@SpringBootApplication 			////similar to @Configuration @EnableAutoConfiguration @ComponentScan 
+@SpringBootApplication 			//// to @Configuration @EnableAutoConfiguration @ComponentScan 
 public class SpringBootExampleSts 
 {
 public static void main(String[] args)
